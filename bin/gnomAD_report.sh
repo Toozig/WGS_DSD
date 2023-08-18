aa6212b4caf0f801b62d9dbbd23f5eda82a44e8e
toozig <toozig@gmail.com>
2023-08-18 14:53:33 +0300

    report works

--- bin/gnomAD_report.sh
+++ bin/gnomAD_report.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+################################################################################
+# Script Name: chromosome_variant_checker.sh
+# Description: This script checks if the latest variant of each chromosome from
+#              a local file is similar to the corresponding variant in the
+#              gnomAD database.
+#
+# Usage: bash chromosome_variant_checker.sh <region_file> <region_simple_name> <local_gnomAD_folder> <output>
+#
+# Arguments:
+#   region_file         Path to the region file containing chromosome information.
+#   region_simple_name  Simple name of the region.
+#   local_gnomAD_folder Path to the folder where local gnomAD data stored
+#   output              Name of the output file to store results.
+#
+# Output:
+#   The script outputs the name of the output file with comparison results.
+
+################################################################################
+
 region_file=$1
 region_simple_name=$2
-output=$3
+local_gnomAD_folder=$3
+output=$4
 
 
 
 standard_chromosomes=("chr1" "chr2" "chr3" "chr4" "chr5" "chr6" "chr7" "chr8" "chr9" "chr10" "chr11" "chr12" "chr13" "chr14" "chr15" "chr16" "chr17" "chr18" "chr19" "chr20" "chr21" "chr22" "chrX" "chrY")
 for  chrom in "${standard_chromosomes[@]}"; do
 
-    local_gnomAD="data/read_only/variant_gnomAD/$region_simple_name/$chrom.$region_simple_name.tsv.gz"
+    local_gnomAD="$local_gnomAD_folder/$chrom.$region_simple_name.tsv.gz"
     # Print the current chromosome being processed
     echo "$chrom"
     
